0db1d3df4ab1f368fabdb39c64ed1839778a3cd2
ShogoTakagi <t1610380@edu.cc.uec.ac.jp>
2021-02-22 21:59:27 +0900

    コルーチンの例

---
+++ Assets/BallAttackgame(MiniGame)/Assets/CoroutineSample.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineSample : MonoBehaviour
+{
+    bool isCollided = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(TimeCoroutine(3.0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator TimeCoroutine(float time)
+    {
+        while (isCollided)
+        {
+            Debug.Log("Loop");
+            yield return new WaitForSeconds(time);
+        }
+        yield return null;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isCollided = true;
+    }
+}
