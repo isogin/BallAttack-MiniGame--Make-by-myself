8497baf7e06415169452b691fbefbe34ab9c5a61
isogin <77102591+isogin@users.noreply.github.com>
2021-03-05 12:59:41 +0900

    MakeStory5
    スキル２の簡易実装
    エネミーのAI簡易実装(ヒットアンドアウェイ)

--- Assets/BallAttackgame(MiniGame)/enemyScripts.cs
+++ Assets/BallAttackgame(MiniGame)/enemyScripts.cs
@@ -4,24 +4,57 @@ using UnityEngine;
 
 public class enemyScripts : MonoBehaviour
 {
-    public Rigidbody rb2;
+    public GameObject player;
+    public float distanceBase = 5f;
+    public bool touchGround;
+    public bool awaynow = false;
+    public float xPos;
+    public float zPos;
+    public Transform awayPos;
+    public GameObject destination;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player");
     }
 
-    // Update is called once per frame
+    // Update is called once p frame
     void Update()
     {
+        float distance = distanceBase * Time.deltaTime;
+
+        if(touchGround  == true && awaynow == false)
+        {
+            this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, player.transform.position, distance);
+            Debug.Log("追いかけ中");
+        }
+        if(awaynow == true)
+        {
+            
+            destination.transform.position = new Vector3(xPos, 0.55f, zPos);
+                this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, destination.transform.position, distance);
+            Debug.Log("逃げ中");
+            if(this.gameObject.transform.position == destination.transform.position)
+            {
+                awaynow = false;
+            }
+            
+        }
         
     }
-    private void OnCollisionEnter(Collision other)
+    private void OnCollisionStay(Collision collision)
     {
-        if(other.gameObject.tag == "playerBall")
+        touchGround = true;
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        touchGround = false;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "PlayerBall")
         {
-            rb2.AddForce(0, 150, 0);
+            awaynow = true;
         }
-       
     }
 }
